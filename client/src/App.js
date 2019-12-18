68603991dc7d3043a37b32220fca4456d0dc4aca
unknown <emilia_nedialkova@abv.bg>
2019-12-18 20:32:49 +0200

    init admin panel

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import Main from './components/Main';
+import  MainUser from './components/Main';
+import MainAdmin from './components/AdminArea/Main';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Main />
+    <Router>
+      <div className="App">
+        <Link to="/" class="btn btn-primary">To Main</Link>
+        <Link to="/admin" class="btn btn-danger">To Admin</Link>
+
+        <Route path='/' exact component={MainUser} />
+        <Route path='/admin' component={MainAdmin} />
     </div>
+    </Router>
   );
 }
 
