712b5d6f48e1b950d379ba1c6694a96183271a17
unknown <emilia_nedialkova@abv.bg>
2019-12-18 23:27:23 +0200

    added some routing between areas

--- client/src/components/AdminArea/Sidebar.jsx
+++ client/src/components/AdminArea/Sidebar.jsx
@@ -1,103 +1,169 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Sidebar = () => (
-    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+class Sidebar extends Component {
+    constructor(props) {
+        super(props);
 
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-    <div class="sidebar-brand-icon rotate-n-15">
-      <i class="fas fa-laugh-wink"></i>
-    </div>
-    <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-  </a>
+        this.state = {
+            isOpened: true
+        };
 
-  <hr class="sidebar-divider my-0" />
+        this.toggleHandler = this.toggleHandler.bind(this);
+    }
 
-  <li class="nav-item active">
-    <a class="nav-link" href="index.html">
-      <i class="fas fa-fw fa-tachometer-alt"></i>
-      <span>Dashboard</span></a>
-  </li>
+    toggleHandler() {
+        console.log(this.state.isOpened);
 
-  <hr class="sidebar-divider" />
+        this.setState({
+            isOpened: !this.state.isOpened
+        });
+    }
 
-  <div class="sidebar-heading">
-    Interface
-  </div>
+    generateClass() {
+        return this.state.isOpened === true ? '' : 'toggled'
+    }
 
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-      <i class="fas fa-fw fa-cog"></i>
-      <span>Components</span>
-    </a>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Custom Components:</h6>
-        <a class="collapse-item" href="buttons.html">Buttons</a>
-        <a class="collapse-item" href="cards.html">Cards</a>
-      </div>
-    </div>
-  </li>
+    /*
+    componentDidUpdate(prevProps, prevState) {
+        console.log(prevState.isOpened, this.state.isOpened);
+    }
+    */
 
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-      <i class="fas fa-fw fa-wrench"></i>
-      <span>Utilities</span>
-    </a>
-    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Custom Utilities:</h6>
-        <a class="collapse-item" href="utilities-color.html">Colors</a>
-        <a class="collapse-item" href="utilities-border.html">Borders</a>
-        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-        <a class="collapse-item" href="utilities-other.html">Other</a>
-      </div>
-    </div>
-  </li>
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log(nextState.isOpened, this.state.isOpened);
+        return nextState.isOpened !== this.state.isOpened
+    }
 
-  <hr class="sidebar-divider"/>
+    render() {
+        console.log('rerendered');
+        return (
+    
+            <ul className={'navbar-nav bg-gradient-primary sidebar sidebar-dark accordion ' + this.generateClass()} id="accordionSidebar">
+        
+          <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <div className="sidebar-brand-icon rotate-n-15">
+              <i className="fas fa-laugh-wink"></i>
+            </div>
+            <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+          </a>
+        
+          <hr className="sidebar-divider my-0" />
+        
+          <li className="nav-item active">
+            <a className="nav-link" href="/admin">
+              <i className="fas fa-fw fa-tachometer-alt"></i>
+              <span>Dashboard</span></a>
+          </li>
+        
+          <hr className="sidebar-divider" />
 
-  <div class="sidebar-heading">
-    Addons
-  </div>
+          <div className="sidebar-heading">
+            Interface
+          </div>
+          
+          <li className="nav-item">
+            <a className="nav-link" href="/admin/shoes">
+            <i class="fas fa-shoe-prints"></i>
+              <span>Shoes</span></a>
+          </li>
 
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-      <i class="fas fa-fw fa-folder"></i>
-      <span>Pages</span>
-    </a>
-    <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Login Screens:</h6>
-        <a class="collapse-item" href="login.html">Login</a>
-        <a class="collapse-item" href="register.html">Register</a>
-        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-        <div class="collapse-divider"></div>
-        <h6 class="collapse-header">Other Pages:</h6>
-        <a class="collapse-item" href="404.html">404 Page</a>
-        <a class="collapse-item" href="blank.html">Blank Page</a>
-      </div>
-    </div>
-  </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+            <i class="fas fa-chevron-circle-left"></i>
+              <span>Back to user area</span></a>
+          </li>
+        
+        
+          
+        
+          <li className="nav-item">
+            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+              <i className="fas fa-fw fa-cog"></i>
+              <span>Components</span>
+            </a>
+            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+              <div className="bg-white py-2 collapse-inner rounded">
+                <h6 className="collapse-header">Custom Components:</h6>
+                <a className="collapse-item" href="buttons.html">Buttons</a>
+                <a className="collapse-item" href="cards.html">Cards</a>
+              </div>
+            </div>
+          </li>
+        
+          <li className="nav-item">
+            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+              <i className="fas fa-fw fa-wrench"></i>
+              <span>Utilities</span>
+            </a>
+            <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+              <div className="bg-white py-2 collapse-inner rounded">
+                <h6 className="collapse-header">Custom Utilities:</h6>
+                <a className="collapse-item" href="utilities-color.html">Colors</a>
+                <a className="collapse-item" href="utilities-border.html">Borders</a>
+                <a className="collapse-item" href="utilities-animation.html">Animations</a>
+                <a className="collapse-item" href="utilities-other.html">Other</a>
+              </div>
+            </div>
+          </li>
+        
+          <hr className="sidebar-divider"/>
+        
+          <div className="sidebar-heading">
+            Addons
+          </div>
+        
+          <li className="nav-item">
+            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+              <i className="fas fa-fw fa-folder"></i>
+              <span>Pages</span>
+            </a>
+            <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+              <div className="bg-white py-2 collapse-inner rounded">
+                <h6 className="collapse-header">Login Screens:</h6>
+                <a className="collapse-item" href="login.html">Login</a>
+                <a className="collapse-item" href="register.html">Register</a>
+                <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
+                <div className="collapse-divider"></div>
+                <h6 className="collapse-header">Other Pages:</h6>
+                <a className="collapse-item" href="404.html">404 Page</a>
+                <a className="collapse-item" href="blank.html">Blank Page</a>
+              </div>
+            </div>
+          </li>
+        
+          <li className="nav-item">
+            <a className="nav-link" href="charts.html">
+              <i className="fas fa-fw fa-chart-area"></i>
+              <span>Charts</span></a>
+          </li>
+        
+          <li className="nav-item">
+            <a className="nav-link" href="tables.html">
+              <i className="fas fa-fw fa-table"></i>
+              <span>Tables</span></a>
+          </li>
+        
+          <hr className="sidebar-divider d-none d-md-block" />
+        
+          <div className="text-center d-none d-md-inline">
+            <button className="rounded-circle border-0" id="sidebarToggle" onClick={this.toggleHandler}></button>
+          </div>
+        
+        </ul>
+        );
+    }
+}
 
-  <li class="nav-item">
-    <a class="nav-link" href="charts.html">
-      <i class="fas fa-fw fa-chart-area"></i>
-      <span>Charts</span></a>
-  </li>
 
-  <li class="nav-item">
-    <a class="nav-link" href="tables.html">
-      <i class="fas fa-fw fa-table"></i>
-      <span>Tables</span></a>
-  </li>
+// const Sidebar = (props) => {
+//     console.log(props);
 
-  <hr class="sidebar-divider d-none d-md-block" />
+//     generateClass = () => {
+//         return this.props.sidebarOpened === true ? '' ? 'toggled';
+//     }
 
-  <div class="text-center d-none d-md-inline">
-    <button class="rounded-circle border-0" id="sidebarToggle"></button>
-  </div>
 
-</ul>
-);
+    
+// }
 
 export default Sidebar;
---
+++ client/src/components/AdminArea/pages/Shoes.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Shoes extends Component {
+    render() {
+        return ( 
+        <h1>Shoes</h1>
+        )
+    }
+}
+
+export default Shoes;
